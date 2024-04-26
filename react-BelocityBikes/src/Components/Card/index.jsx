089f9fb40926d97d6f471a210495c6212d58761f
Alberto_Samayoa <78399244+Sama7u7@users.noreply.github.com>
2024-04-26 01:59:12 -0600

    Consumo de la Fake Store Api

--- react-BelocityBikes/src/Components/Card/index.jsx
+++ react-BelocityBikes/src/Components/Card/index.jsx
@@ -1,16 +1,16 @@
-const Card = () => {
+const Card = (data) => {
     return(
         <div className='bg-white cursor-pointer w-56 h-68'> 
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 rounded-sm bg-neutral-100 text-black text-xs '>MTB</span>
-                    <img  className='w-full h-full object-cover rounded-lg ' src="https://images2.giant-bicycles.com/b_white%2Cc_pad%2Ch_400%2Cq_80%2Cw_600/pnft7kcgiy2qirrrkcb9/MY22Fathom291_ColorAAmberGlow.jpg" alt='Fathon'/>
+                <span className='absolute bottom-0 left-0 rounded-sm bg-neutral-100 text-black text-xs '>{data.data.category}</span>
+                    <img  className='w-full h-full object-cover rounded-lg ' src={data.data.image} alt={data.data.description}/>
                     <div className="absolute top-0 right-0 flex justify-center items-center bg-gray-400 w-6 h-6 rounded-full m-2">
                         +
                     </div>
             </figure>
             <p className='flex justify-between'> 
-                <span className='text-sm font-light'>Fathon 29 1</span>
-                <span  className='text-sm font-bold'>$35,900 </span>
+                <span className='text-sm font-light'>{data.data.title}</span>
+                <span  className='text-sm font-bold'>${data.data.price}</span>
             </p>
         </div>
     )
--- react-BelocityBikes/src/Pages/Home/index.jsx
+++ react-BelocityBikes/src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() =>{
+    fetch('https://fakestoreapi.com/products')
+     .then(response =>response.json())
+     .then(data => setItems(data))
+  }, []) 
+
   return (
+    
     <Layout>
        Home
-       <Card></Card>
+       <div className='grid gap-4  grid-cols-2 sm:grid-cols-4 w-full max-w-screen-lg px-2'>
+       {
+        items?.map((item)=> (
+         <Card key={item.id} data={item}/>
+        )) 
+       }
+       </div>
     </Layout>
   )
 }
